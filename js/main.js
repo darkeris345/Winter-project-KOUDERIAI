9c574e9036334e1fc9a25d58fb376da9ca8d4968
darkeris345 <dimuxxx72@gmail.com>
2023-11-29 14:52:42 +0200

    Hope it's final navbar push

--- js/main.js
+++ js/main.js
@@ -15,5 +15,13 @@ function toggleMenu() {
     menuIcon.style.display = "none";
   }
 }
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
 
 hamburger.addEventListener("click", toggleMenu);
