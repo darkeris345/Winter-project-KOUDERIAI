c473f7f5b793d0e0011cf3fa086bfbd0c42cf6f5
ugggie <ugne.kalantaite@stud.techin.lt>
2023-12-06 14:06:42 +0200

    Pages no longer refresh when submit button is clicked, JS file prevenRefresh added

---
+++ js/preventRefresh.js
@@ -0,0 +1,6 @@
+document.querySelector('.no__refresh').addEventListener('submit', function(event){
+    event.preventDefault()
+  });
+  document.querySelector('.no__refresh1').addEventListener('submit', function(event){
+    event.preventDefault()
+  });
